c1e670bdcd099e807111a3ce0ec837219efe8128
rasto <rasto.kopal@gmail.com>
2023-04-13 15:16:36 +0200

    added vote test to test increment of nPositiveVotes in aclaims etc, added test for votes-review

--- src/api/tests/integration/vote.test.js
+++ src/api/tests/integration/vote.test.js
@@ -10,6 +10,22 @@ const Claim = require('../../models/claim.model');
 const Review = require('../../models/review.model');
 const Vote = require('../../models/vote.model');
 
+const reviewById = async (articleId, claimId, reviewId, accessToken) => {
+  return request(app)
+    .get(`/v1/articles/${articleId}/claims/${claimId}/reviews/${reviewId}`)
+    .set('Authorization', `Bearer ${accessToken}`)
+    .expect(httpStatus.OK)
+    .then((res) => res.body);
+};
+
+const getClaimById = async (articleId, claimId, accessToken) => {
+  return request(app)
+    .get(`/v1/articles/${articleId}/claims/${claimId}`)
+    .set('Authorization', `Bearer ${accessToken}`)
+    .expect(httpStatus.OK)
+    .then((res) => res.body);
+};
+
 describe('Vote API', async () => {
   let userAccessToken;
   let user2AccessToken;
@@ -19,7 +35,11 @@ describe('Vote API', async () => {
   let article2;
   let claim1;
   let claim2;
+  let review1;
+  let review2;
   let positiveVote;
+  let negVote;
+  let neutralVote;
 
   before(async () => {
     await Vote.deleteMany({});
@@ -32,6 +52,14 @@ describe('Vote API', async () => {
   beforeEach(async () => {
     positiveVote = {
       rating: 1,
+    };
+
+    negVote = {
+      rating: -1,
+    };
+
+    neutralVote = {
+      rating: 0,
     };
 
     user1 = {
@@ -77,6 +105,9 @@ describe('Vote API', async () => {
       articleId: article1._id,
       _id: '41224d776a326fb40f000008',
       text: 'Prvy claim hh nejaky nahodny text. Nema to ziadny zmysel, ale vsak to nie je podsatatne..',
+      nPositiveVotes: 0,
+      nNegativeVotes: 0,
+      nBeenVoted: 0,
     };
 
     claim2 = {
@@ -84,8 +115,40 @@ describe('Vote API', async () => {
       articleId: article1._id,
       _id: '41224d776a326fb40f000009',
       text: 'Prvy claim hh nejaky nahodny text. Nema to ziadny zmysel, ale vsak to nie je podsatatne..',
+      nPositiveVotes: 0,
+      nNegativeVotes: 0,
+      nBeenVoted: 0,
     };
 
+    review1 = {
+      _id: '21224d776a326fb40f000003',
+      userId: user1._id,
+      claimId: claim1._id,
+      articleId: article1._id,
+      vote: 'positive',
+      links: ['factcheck.cz', 'redirect.com', 'https://www.youtube.c'],
+      text: 'Prvy claim hh nejaky nahodny text. Nema to ziadny zmysel, ale vsak to nie je podsatatne..',
+      nPositiveVotes: 0,
+      nNegativeVotes: 0,
+      nNeutralVotes: 0,
+      nBeenVoted: 0,
+    };
+
+    review2 = {
+      _id: '21224d776a326fb40f100003',
+      userId: user1._id,
+      claimId: claim1._id,
+      articleId: article1._id,
+      vote: 'negative',
+      links: ['factddcheck.cz', 'rrredirect.com', 'https://www.youtube.c'],
+      text: 'Prvy claim hh nejaky nahodny text. Nema to ziadny zmysel, ale vsak to nie je podsatatne..',
+      nPositiveVotes: 0,
+      nNegativeVotes: 0,
+      nNeutralVotes: 0,
+      nBeenVoted: 0,
+    };
+
+    await Review.deleteMany({});
     await Claim.deleteMany({});
     await Article.deleteMany({});
     await User.deleteMany({});
@@ -99,39 +162,84 @@ describe('Vote API', async () => {
     await Article.create(article2);
     await Claim.create(claim1);
     await Claim.create(claim2);
+    await Review.create(review1);
+    await Review.create(review2);
   });
 
   describe('POST /v1/vote?claimId=', async () => {
-    it('user1 should vote for an claim1', () => {
+    it('user1 should vote for an claim1', async () => {
       return request(app)
         .post(`/v1/vote?claimId=${claim1._id}`)
         .set('Authorization', `Bearer ${userAccessToken}`)
         .send(positiveVote)
         .expect(httpStatus.CREATED)
-        .then((res) => {
+        .then(async (res) => {
           expect(res.body.rating).to.be.equal(1);
+        })
+        .then(async () => {
+          const claim = await getClaimById(article1._id, claim1._id, userAccessToken);
+          expect(claim.nPositiveVotes).to.be.equal(claim1.nPositiveVotes + 1);
+          expect(claim.nNegativeVotes).to.be.equal(claim1.nNegativeVotes);
+          expect(claim.nBeenVoted).to.be.equal(claim1.nBeenVoted + 1);
+          claim1.nPositiveVotes += 1;
+          claim1.nBeenVoted += 1;
         });
     });
 
-    it('user1 should vote for an claim2', () => {
+    it('user1 should vote for an claim2', async () => {
       return request(app)
         .post(`/v1/vote?claimId=${claim2._id}`)
         .set('Authorization', `Bearer ${userAccessToken}`)
         .send(positiveVote)
         .expect(httpStatus.CREATED)
-        .then((res) => {
+        .then(async (res) => {
           expect(res.body.rating).to.be.equal(1);
+        })
+        .then(async () => {
+          const claim = await getClaimById(article1._id, claim2._id, userAccessToken);
+          expect(claim.nPositiveVotes).to.be.equal(claim2.nPositiveVotes + 1);
+          expect(claim.nNegativeVotes).to.be.equal(claim2.nNegativeVotes);
+          expect(claim.nBeenVoted).to.be.equal(claim2.nBeenVoted + 1);
+          claim2.nPositiveVotes += 1;
+          claim2.nBeenVoted += 1;
         });
     });
 
-    it('user2 should vote for an claim1', () => {
+    it('user2 should vote for an claim1', async () => {
       return request(app)
         .post(`/v1/vote?claimId=${claim1._id}`)
         .set('Authorization', `Bearer ${user2AccessToken}`)
         .send(positiveVote)
         .expect(httpStatus.CREATED)
-        .then((res) => {
+        .then(async (res) => {
           expect(res.body.rating).to.be.equal(1);
+        })
+        .then(async () => {
+          const claim = await getClaimById(article1._id, claim1._id, userAccessToken);
+          expect(claim.nPositiveVotes).to.be.equal(claim1.nPositiveVotes + 1);
+          expect(claim.nNegativeVotes).to.be.equal(claim1.nNegativeVotes);
+          expect(claim.nBeenVoted).to.be.equal(claim1.nBeenVoted + 1);
+          claim1.nPositiveVotes += 1;
+          claim1.nBeenVoted += 1;
+        });
+    });
+
+    it('user2 should give negative vote for claim2', async () => {
+      return request(app)
+        .post(`/v1/vote?claimId=${claim2._id}`)
+        .set('Authorization', `Bearer ${user2AccessToken}`)
+        .send(negVote)
+        .expect(httpStatus.CREATED)
+        .then(async (res) => {
+          expect(res.body.rating).to.be.equal(-1);
+        })
+        .then(async () => {
+          const claim = await getClaimById(article1._id, claim2._id, userAccessToken);
+          expect(claim.nPositiveVotes).to.be.equal(claim2.nPositiveVotes);
+          expect(claim.nNegativeVotes).to.be.equal(claim2.nNegativeVotes + 1);
+          expect(claim.nBeenVoted).to.be.equal(claim2.nBeenVoted + 1);
+          claim2.nNegativeVotes += 1;
+          claim2.nBeenVoted += 1;
         });
     });
 
@@ -182,8 +290,112 @@ describe('Vote API', async () => {
     });
   });
 
+  describe('POST /v1/vote?reviewId=', async () => {
+    it('user1 should give neutral vote for an review1', async () => {
+      return request(app)
+        .post(`/v1/vote?reviewId=${review1._id}`)
+        .set('Authorization', `Bearer ${userAccessToken}`)
+        .send(neutralVote)
+        .expect(httpStatus.CREATED)
+        .then(async (res) => {
+          expect(res.body.rating).to.be.equal(0);
+        })
+        .then(async () => {
+          const review = await reviewById(article1._id, claim1._id, review1._id, userAccessToken);
+          expect(review.nPositiveVotes).to.be.equal(review1.nPositiveVotes);
+          expect(review.nNeutralVotes).to.be.equal(review1.nNeutralVotes + 1);
+          expect(review.nNegativeVotes).to.be.equal(review1.nNegativeVotes);
+          expect(review.nBeenVoted).to.be.equal(review1.nBeenVoted + 1);
+          review1.nNeutralVotes += 1;
+          review1.nBeenVoted += 1;
+        });
+    });
+
+    it('user1 should give negative vote for an review2', async () => {
+      return request(app)
+        .post(`/v1/vote?reviewId=${review2._id}`)
+        .set('Authorization', `Bearer ${userAccessToken}`)
+        .send(negVote)
+        .expect(httpStatus.CREATED)
+        .then(async (res) => {
+          expect(res.body.rating).to.be.equal(-1);
+        })
+        .then(async () => {
+          const review = await reviewById(article1._id, claim1._id, review2._id, userAccessToken);
+          expect(review.nPositiveVotes).to.be.equal(review2.nPositiveVotes);
+          expect(review.nNeutralVotes).to.be.equal(review2.nNeutralVotes);
+          expect(review.nNegativeVotes).to.be.equal(review2.nNegativeVotes + 1);
+          expect(review.nBeenVoted).to.be.equal(review2.nBeenVoted + 1);
+          review2.nNegativeVotes += 1;
+          review2.nBeenVoted += 1;
+        });
+    });
+
+    it('user2 should give neutral vote for an review1', async () => {
+      return request(app)
+        .post(`/v1/vote?reviewId=${review1._id}`)
+        .set('Authorization', `Bearer ${user2AccessToken}`)
+        .send(neutralVote)
+        .expect(httpStatus.CREATED)
+        .then(async (res) => {
+          expect(res.body.rating).to.be.equal(0);
+        })
+        .then(async () => {
+          const review = await reviewById(article1._id, claim1._id, review1._id, userAccessToken);
+          expect(review.nPositiveVotes).to.be.equal(review1.nPositiveVotes);
+          expect(review.nNeutralVotes).to.be.equal(review1.nNeutralVotes + 1);
+          expect(review.nNegativeVotes).to.be.equal(review1.nNegativeVotes);
+          expect(review.nBeenVoted).to.be.equal(review1.nBeenVoted + 1);
+          review1.nNeutralVotes += 1;
+          review1.nBeenVoted += 1;
+        });
+    });
+
+    it('user2 should give positive vote for review2', async () => {
+      return request(app)
+        .post(`/v1/vote?reviewId=${review2._id}`)
+        .set('Authorization', `Bearer ${user2AccessToken}`)
+        .send(positiveVote)
+        .expect(httpStatus.CREATED)
+        .then(async (res) => {
+          expect(res.body.rating).to.be.equal(1);
+        })
+        .then(async () => {
+          const review = await reviewById(article1._id, claim1._id, review2._id, userAccessToken);
+          expect(review.nPositiveVotes).to.be.equal(review2.nPositiveVotes + 1);
+          expect(review.nNegativeVotes).to.be.equal(review2.nNegativeVotes);
+          expect(review.nBeenVoted).to.be.equal(review2.nBeenVoted + 1);
+          review2.nPositiveVotes += 1;
+          review2.nBeenVoted += 1;
+        });
+    });
+
+    it('should return not_found when id does not exist', () => {
+      return request(app)
+        .post('/v1/vote?reviewId=41224d776a326fb40f010090')
+        .set('Authorization', `Bearer ${userAccessToken}`)
+        .send(neutralVote)
+        .expect(httpStatus.NOT_FOUND);
+    });
+
+    it('should return error when rating is not specified', () => {
+      return request(app)
+        .post(`/v1/vote?reviewId=${review2._id}`)
+        .set('Authorization', `Bearer ${userAccessToken}`)
+        .send({ text: 'hehe' })
+        .expect(httpStatus.BAD_REQUEST)
+        .then((res) => {
+          const { field, location, messages } = res.body.errors[0];
+
+          expect(field).to.be.equal('rating');
+          expect(location).to.be.equal('body');
+          expect(messages).to.include('"rating" is required');
+        });
+    });
+  });
+
   describe('POST /v1/vote?articleId=', async () => {
-    it('user1 should vote for an article1', () => {
+    it('user1 should vote for an article1', async () => {
       return request(app)
         .post(`/v1/vote?articleId=${article1._id}`)
         .set('Authorization', `Bearer ${userAccessToken}`)
